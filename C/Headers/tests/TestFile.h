b19984567c610e5a480c403c22d4772508575e62
Brooke Cronin <brookesynchro123@gmail.com>
2023-11-23 15:03:40 -0500

    initial commit

---
+++ C/Headers/tests/TestFile.h
@@ -0,0 +1,12 @@
+
+#pragma once
+
+#include <stdio.h>
+
+int sum(int a, int b);
+
+double difference(double a, double b);
+
+void printString(char* string);
+
+bool isAwesome(int me);
---
+++ Java/tests/TestFile.java
@@ -0,0 +1,20 @@
+
+package Java.tests;
+
+public class TestFile {
+    int sum(int a, int b)
+    {
+        for(int i = 0; i < 2; i++)
+        {
+
+        }
+        return a+b;
+    }
+    public static double difference(double a, double b)
+    {
+        return a-b;
+    }
+	public static void main(String[] args) {
+        
+    }
+}
