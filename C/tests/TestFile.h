6bdcd3d41a681a6a72206475d61cce92d1ca643c
Brooke Cronin <brookesynchro123@gmail.com>
2023-11-23 17:29:20 -0500

    added for .c /.h files & cmd line for them

--- C/tests/TestFile.h
+++ C/tests/TestFile.h
@@ -2,11 +2,12 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdbool.h>
 
 int sum(int a, int b);
 
 double difference(double a, double b);
 
-void printString(char* string);
+void printString(char** string);
 
 bool isAwesome(int me);
